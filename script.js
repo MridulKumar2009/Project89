86079c6444556df526b9b5a8ee9ddbe7b1ffeeb4
MridulKumar2009 <89531989+MridulKumar2009@users.noreply.github.com>
2022-04-27 20:09:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,4 @@ function adduser(){
     player2_name=document.getElementById("player_name_input2").value;
     localStorage.setItem("player1_name", player1_name);
     localStorage.setItem("player2_name", player2_name);
-    window.location="game_page.html";
 }
